6d540446cd264a42180f673d64142f8501192a31
BangDe <bangde@localhost.localdomain>
2020-10-02 22:35:42 +0700

    video 3, pengenalan tipe data

---
+++ js/tipedata.js
@@ -0,0 +1,13 @@
+let angka = 10;
+let kata = "Saya belajar javascript";
+let pecahan = 2.5;
+
+// penggunaan (+)
+// 1. kedua variable adalah number maka hasilnya akan menjumlahkan variable
+// 2. kedua variable adalah string maka hasilnya kedua sting akan bergabung menjadi satu string
+// 3. ketika variable string dan number bertemu operator bilangan selain (+) maka hasilnya adalah Nan
+// 4. Jika ingin menggabungkan string dan number jangan menggunakan operator bilangan selain (+)
+        // contoh : console.log("Saya berumur " + 19); => Saya berumur 19
+// 5. Menggabungkan menggunakan backtics 
+        // contoh : console.log( ${variableString} ${variableNumber} ) maka hasilnya adlah digabungkan
+// 6. Tipe data lain yang bisa digunakan adalah bolean(ture/false), null, undefined
