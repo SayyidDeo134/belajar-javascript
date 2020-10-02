8ef6398f2bc38d737ca3c90f219de954179f5ebc
BangDe <bangde@localhost.localdomain>
2020-10-02 21:59:03 +0700

    Video 2, Pembelajaran variable dan konstanta

---
+++ js/variabel.js
@@ -0,0 +1,14 @@
+// variabel pada javascript
+// ketika variabel tidak di isi / hanya dideklarasikan akan mengembalikan nilai undefined
+let angka = 20;
+let kata = "Saya belajar";
+
+// konstanta
+// syarat konstanta adalah harus di isi ketika sudah dideklarasikan
+// nilai konstanta tidak bisa diganti / bernilai tetap
+const data = 10;
+
+// untuk melihat type data dari variabel
+// menggunakan (typeof)
+// let coba = "Coba string";
+// console.log(typeof coba);
