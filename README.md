46955e766796df48cb12ebf5954d3769e0d1799d
BangDe <bangde@localhost.localdomain>
2020-10-28 08:06:08 +0700

    Video 4, Operator Logika

---
+++ README.md
@@ -0,0 +1,18 @@
+BELAJAR JAVASCRIPT
+
+SMK REVIT
+
+VIDEO 1 : Penulisan coding
+    Mengerti cara penulisan coding javascript dengan internal maupun eksternal
+
+VIDEO 2 : Variabel dan konstanta
+    Mengerti dan mengenal variabel, konstanta dan cara penulisan
+
+VIDEO 3 : Tipe Data
+    Mengerti dan mengenal tipe data pada javascript
+
+VIDEO 4 : Operator Logika
+    Mengerti dan mengenal operator matematika pada javascript
+
+VIDEO 5 : Operator Logika
+    
---
+++ js/opertor.js
@@ -0,0 +1,33 @@
+// Metode pendeklarasian variable
+// pertama
+let p, l, t, hasil;
+// kedua
+let panjang = 5, lebar = 3, tinggi = 4;
+// ketiga
+let pan = 5;
+let leb = 3;
+let tin = 4;
+
+// Operator matematika
+// +, -, /, *, %
+
+hasil = pan + leb;
+// hasilnya adalah 8
+hasil = pan - tin;
+// hasilnya adalah 1
+hasil = tin / leb;
+// hasilnya adalah 1.33
+hasil = pan % tin;
+// hasilnya adalah 1
+
+// Operator (+) bisa digunakan untuk 
+// 1. Penggabungan 2 string
+// 2. Penggabungan string dengan integer
+
+let sidoarjo = "Sido" + "arjo";
+// hasilnya adalah "Sidoarjo"
+let satuLima = "1" + 5;
+// hasilnya adalah 15
+
+
+
