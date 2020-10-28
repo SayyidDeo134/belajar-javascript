4e23d6c194814fd8b70c1fd8cdf994e347a0455b
BangDe <bangde@localhost.localdomain>
2020-10-28 08:31:27 +0700

    Video 6, percabangan if

---
+++ js/percabangan.js
@@ -0,0 +1,13 @@
+// Percabangan pada Javascript
+// Penggunaan if
+// if(pengujian){
+//  Hail pengujian jika bernilai true
+// }else{
+//  Hasil pengujian jika bernilai false
+// }
+
+if (true) {
+    console.log('Hasil pengujian yang berilai true');
+} else {
+    console.log('Hasil pengujian yang berniali false');
+}
